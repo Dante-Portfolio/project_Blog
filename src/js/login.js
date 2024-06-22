5e9b3701809ee52de70fa20635a5cd6c2b482196
Dante <ajsantos.082@gmail.com>
2024-06-23 00:20:22 +0200

    feat: LOGIN send form data

---
+++ src/js/login.js
@@ -0,0 +1,32 @@
+const user = document.getElementById('user')
+const pass = document.getElementById('pass')
+const accept = document.getElementById('accept')
+const send = document.getElementById('send')
+// send.preventDefault
+
+function changeSubmit() {
+    if (send.disabled) {
+        send.removeAttribute('disabled')
+    } else {
+        send.disabled = true
+    }
+}
+
+function testForm() {
+    if (user.value.split(' ').length != 1 || pass.value.split(' ').length != 1) {
+        alert('Los espacios no están permitidos')
+    }
+    if (user.value.length > 0 && pass.value.length > 0) {
+        redirection('passed')
+    } else {
+        alert('revisa todos los campos')
+    }
+}
+
+function redirection(param) {
+    if (param == 'passed') {
+        window.open('../html/auth.html?user=' + user.value + '&' + pass.value)
+    }
+}
+accept.onclick = changeSubmit
+send.addEventListener('click', testForm)
